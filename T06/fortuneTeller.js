3e8f86a37340b30b485227ee900f58b643c73041
parmaninisha05 <165329779+parmaninisha05@users.noreply.github.com>
2024-05-16 20:48:53 +0100

    Add files via upload

---
+++ T06/fortuneTeller.js
@@ -0,0 +1,9 @@
+let name = prompt("Enter your Mother's first name: "); 
+let street = prompt("Enter your street where you grew up: ");
+let colour = prompt("Enter your favourite colour as a child: ");
+let age = prompt("Enter your current age: ");
+let number = prompt("Enter number between 1 to 10: ");
+
+console.log(name, age);
+if number=5
+then name = 1 + 2
